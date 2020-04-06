7562e9346dde941a5766dd0a35b696482f5e10f8
Dhosio <danielgithiomi@gmail.com>
2020-04-06 11:03:23 +0300

    Auto-generated commit

--- src/app/Models/MealsArrayModel/meals-array-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MealsArrayModel } from './meals-array-model';
-
-describe('MealsArrayModel', () => {
-  it('should create an instance', () => {
-    expect(new MealsArrayModel()).toBeTruthy();
-  });
-});
--- src/app/Models/MealsArrayModel/meals-array-model.ts
+++
@@ -1,8 +0,0 @@
-export class MealsArrayModel {
-
-   showMealDetails:boolean;
-
-   constructor( public foodname : string, public calories : number, public details : string){
-      this.showMealDetails = false;
-   }
-}
